7df9e89b9f736bfd7c6b208926c723308cd9d7c9
m.ezzeddine <mazen.ezzeddine@hps-worldwide.com>
2021-04-05 12:34:03 +0200

    producer varying workload

--- src/main/java/org/hps/KafkaProducerExample.java
+++ src/main/java/org/hps/KafkaProducerExample.java
@@ -9,6 +9,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -18,18 +20,25 @@ import java.util.concurrent.atomic.AtomicLong;
 public class KafkaProducerExample {
     private static final Logger log = LogManager.getLogger(KafkaProducerExample.class);
 
+    private static Instant start = null;
+
     public static void main(String[] args) throws InterruptedException {
+
+
+
         KafkaProducerConfig config = KafkaProducerConfig.fromEnv();
 
         log.info(KafkaProducerConfig.class.getName() + ": {}", config.toString());
 
         Properties props = KafkaProducerConfig.createProperties(config);
-
+        int delay = config.getDelay();
         KafkaProducer producer = new KafkaProducer(props);
         log.info("Sending {} messages ...", config.getMessageCount());
         boolean blockProducer = System.getenv("BLOCKING_PRODUCER") != null;
 
         AtomicLong numSent = new AtomicLong(0);
+
+        start = Instant.now();
         for (long i = 0; i < config.getMessageCount(); i++) {
 
             log.info("Sending messages \"" + config.getMessage() + " - {}\"{}", i);
@@ -49,10 +58,41 @@ public class KafkaProducerExample {
                 numSent.incrementAndGet();
             }
 
-            Thread.sleep(config.getDelay());
+            Thread.sleep(delay);
+            Instant now = Instant.now();
+
+            long elapsedTime = Duration.between(start, now).toMinutes();
+            if(elapsedTime >= 2) {
+                delay = 500;
+            }
+
+             if(elapsedTime >= 5) {
+                delay = 333;
+            }
+
+             if(elapsedTime >= 7) {
+                delay = 250;
+            }
+
+            if(elapsedTime >= 8) {
+                delay = 100;
+            }
+
+            if(elapsedTime >= 9) {
+                delay = 2000;
+                start = Instant.now();
+            }
+
+
+
+
         }
         log.info("{} messages sent ...", numSent.get());
         producer.close();
         System.exit(numSent.get() == config.getMessageCount() ? 0 : 1);
     }
+
+
+
+
 }
